1ba959b0943dcd7ed181fbabeb2b133f08e8342a
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-03-08 10:12:32 -0500

    downloaded vue
    through vite

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 v-if="selected === 'A'">Correct Answer</h1>
+  <h1 v-else-if="selected === ''">Select an asnwer</h1>
+  <h1 v-else>"Wrong"</h1>
+  <h1>{{ student }}</h1>
+  <ul>
+    <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+  </ul>
+  <select v-model="selected">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <span>{{ selected }}</span>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      student: "Jason",
+      animals: ["pig", "cow", "ostrich", "Zee"],
+      graduated: true,
+      selected: "",
+    };
+  },
+  methods: {},
+};
+</script>
