da8557c70866e0989d47430919fd66d09dd6d046
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-03-29 10:13:00 -0400

    Adding a card

---
+++ vue-project/src/components/TheCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <img src="" alt="" />
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    title: String,
+    description: String,
+    image: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
+
+<style>
+.card {
+  width: 30%;
+  background-color: red;
+  height: 500px;
+  margin: 10px a;
+}
+</style>
