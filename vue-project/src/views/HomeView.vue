ef180bb29f44ec7843b1bbe7f49c32e71ba5fc7b
fannyl-jpg <113190135+fannyl-jpg@users.noreply.github.com>
2023-03-30 09:42:28 -0400

    Revert "Adding a card"
    This reverts commit da8557c70866e0989d47430919fd66d09dd6d046.

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <h1 v-if="selected === 'A'">Correct Answer</h1>
+  <h1 v-if="selected === 'A'">Correct Answer</h1>
   <h1 v-else-if="selected === ''">Select an asnwer</h1>
   <h1 v-else>"Wrong"</h1>
   <h1>{{ student }}</h1>
@@ -12,32 +12,9 @@
     <option>C</option>
   </select>
   <span>{{ selected }}</span>
- -->
-  <div class="home">
-    <Button @button-click="bye">Button One</Button>
-    <Button @button-click="hello">Button Two</Button>
-    <Card title="Hello World" />
-  </div>
 </template>
-
 <script>
-/* import Button from "../components/TheButton.vue";
- */ import Card from "../components/TheCard.vue";
-
 export default {
-  name: "Home",
-  components: {
-    Button,
-    Card,
-  },
-  data() {
-    return {
-      selected: "",
-    };
-  },
-};
-
-/* export default {
   data() {
     return {
       student: "Jason",
@@ -47,8 +24,7 @@ export default {
     };
   },
   methods: {},
-}; */
-
+};
 fetch("https://www.fruityvice.com/api/fruit/banana")
   .then((response) => response.json())
   .then((data) => console.log(data))
